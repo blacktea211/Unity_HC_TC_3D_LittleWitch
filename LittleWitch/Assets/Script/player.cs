3cf8f712b3f7c0d619aa8f0482f8db2607442459
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2021-02-04 20:26:28 +0800

    玩家移動及跳躍

--- LittleWitch/Assets/Script/player.cs
+++ LittleWitch/Assets/Script/player.cs
@@ -11,6 +11,13 @@ public class player : MonoBehaviour
     public float turn = 10;
     [Header("攝影機角度限制")]
     public Vector2 camLimit = new Vector2(-20, 0);
+    [Header("角色旋轉速度")]
+    public float turnspeed = 10;
+    [Header("檢查地板球體半徑")]
+    public float radius = 1f;
+    [Header("檢查地板球體位移")]
+    public Vector3 offset;
+
 
     /// <summary>
     /// 判斷是否落地
@@ -22,19 +29,33 @@ public class player : MonoBehaviour
     private float x;
     private float y;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);                       // 射線顏色 50% 紅
+        Gizmos.DrawSphere(transform.position + offset, radius);     // 畫球體 ( 中心點 + 位移 ,半徑 )   
+                                                                    // 有些角色模組的中心點在身體不在雙腳附近，可從介面調整位移將球體射線移到雙腳
+    }
 
     // 喚醒事件：在 Start 前執行一次
     private void Awake()
     {
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
-        cam = GameObject.Find("攝影機根物件").transform;
+        cam = GameObject.Find("攝影機根物件").transform;    
     }
 
     private void Update()
     {
         Move();
         Turncamera();
+    }
+
+    /// <summary>
+    /// 固定更新事件：50FPS
+    /// </summary>
+    private void FixedUpdate()
+    {
+        Jump();
     }
 
 
@@ -46,16 +67,17 @@ public class player : MonoBehaviour
         float v = Input.GetAxis("Vertical");        // 取得 前後軸 值 W S 上下
         float h = Input.GetAxis("Horizontal");      // 取得 前後軸 值 A D 左右
 
-        #region 以下沒執行動作!!!
+        
 
         // 攝影機旋轉角度後前進，玩家也跟著旋轉後的前進方向移動
         Transform camNew = cam;                                               // 新攝影機角度
         camNew.eulerAngles = new Vector3(0, cam.eulerAngles.y, 0);            // 去掉 x 與 z 角度
 
-        #endregion
+        // 角色的角度 = 角色 , 攝影機 , 角色的差值
+        transform.rotation = Quaternion.Lerp(transform.rotation, camNew.rotation, 0.5f * turnspeed * Time.deltaTime);
 
-        // 加速度 = ( 前方 * 前後值 + 右方 * 左右值 ) * 速度 * 1/60 + 上方 * 加速度上下值
-        rig.velocity = ((transform.forward * v + transform.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
+        // 加速度 = ( 新攝影機角度 前方 * 前後值 + 右方 * 左右值 ) * 速度 * 1/60 + 上方 * 加速度上下值
+        rig.velocity = ((cam.forward * v + cam.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
         
         // 動畫.設定布林值 ( "參數名稱" , 鋼體.加速度.值 > 0 )
         ani.SetBool("跑步開關", rig.velocity.magnitude > 0);
@@ -71,5 +93,21 @@ public class player : MonoBehaviour
         y = Mathf.Clamp(y, camLimit.x, camLimit.y);                 // 限制y
         cam.localEulerAngles = new Vector3(y, x, 0);                // 攝影機.角度 ( y值 , x值 , 0)
         
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)    // 按空白鍵且在地上
+        {
+           rig.AddForce(Vector3.up * jump);                 // 向上 推力
+        }
+
+        // 碰撞物件陣列 = 物理 . 球體碰撞範圍 (中心點 , 半徑 , 圖層)
+        Collider[] hit = Physics.OverlapSphere(transform.position + offset, radius, 1 << 8);
+        if (hit.Length > 0 && hit[0]) onGround = true;      // 如果 碰到物件陣列數量 > 0 且存在 ， 就設定在地面上
+        else onGround = false;                              // 球體碰到地面，就設定為 不在地上
     }
 }
